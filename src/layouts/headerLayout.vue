6001f26e3368b8575c1054373742063eb5c6c2a8
lzyjin <1247y.j@gmail.com>
2023-01-17 22:35:41 +0900

    메뉴 만듦

--- src/layouts/headerLayout.vue
+++ src/layouts/headerLayout.vue
@@ -2,27 +2,44 @@
   <div>
     <header>
       <div class="wrap">
-        <p class="home">Diary</p>
-        <div class="ham">
-          <span></span>
-          <span></span>
-          <span></span>
+        <p class="home">다이어리</p>
+        <div class="ham" @click="openMenu">
+          <i class="xi-bars"></i>
         </div>
       </div>
     </header>
 
-<!--    <div class="menu">-->
-<!--      <nav>-->
+    <div class="menu" v-if="menuState">
+      <div class="top">
+        <div class="btn-close" @click="closeMenu">
+          <i class="xi-close"></i>
+        </div>
+      </div>
+      <nav>
 <!--        <a href="">회원가입</a>-->
 <!--        <a href="">로그아웃</a>-->
-<!--      </nav>-->
-<!--    </div>-->
+        <router-link></router-link>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "headerLayout"
+  name: "headerLayout",
+  data() {
+    return {
+      menuState: false,
+    }
+  },
+  methods: {
+    openMenu() {
+      this.menuState = true;
+    },
+    closeMenu() {
+      this.menuState = false;
+    },
+  },
 }
 </script>
 
