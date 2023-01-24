c0a31a862d416a4f251e7a47c43264cba510bc39
lzyjin <1247y.j@gmail.com>
2023-01-24 17:43:51 +0900

    vuex 에러 고침 (상현 도움)

--- src/views/user/signIn/index.vue
+++ src/views/user/signIn/index.vue
@@ -15,7 +15,7 @@
           <p class="alert"></p>
         </div>
         <div class="item">
-<!--          {{ count }}-->
+          {{ count }}
           <button type="button" @click="increment">로그인</button>
           <router-link to="/signup" class="link" @click="decrement">회원가입은 여기로</router-link>
         </div>
