ac14f4c2bfbac2b71660b9a0e439cb70d61ee76e
lzyjin <1247y.j@gmail.com>
2023-01-25 22:25:49 +0900

    회원가입 정규표현식

--- src/views/user/signUp/index.vue
+++ src/views/user/signUp/index.vue
@@ -6,16 +6,16 @@
       <form action="">
         <div class="item">
           <p class="label">아이디</p>
-          <input type="text" class="data" />
-          <p class="alert"></p>
+          <input type="text" class="data" v-model="id" @input="validateId"/>
+          <p class="alert">{{ alertId }}</p>
         </div>
         <div class="item">
           <p class="label">비밀번호</p>
-          <input type="text" class="data" />
-          <p class="alert"></p>
+          <input type="text" class="data" v-model="password" @input="validatePassword"/>
+          <p class="alert">{{ alertPassword }}</p>
         </div>
         <div class="item">
-          <button type="button">회원가입</button>
+          <button type="button" @click="signUp">회원가입</button>
         </div>
       </form>
     </div>
@@ -25,7 +25,37 @@
 <script>
 export default {
   name: "index",
+  data() {
+    return {
+      id: '',
+      password: '',
+      alertId: '',
+      alertPassword: '',
+    }
+  },
+  methods: {
+    validateId() {
+      // 알파벳 대소문자 또는 숫자, 6 ~ 10자리
+      const regExp = /^[a-zA-Z0-9]{6,10}$/;
+      if (!regExp.test(this.id)) {
+        this.alertId = '아이디는 6~10글자의 영문 대소문자 또는 숫자의 조합이어야 합니다.';
+      } else {
+        this.alertId = '';
+      }
+    },
+    validatePassword() {
+      // 최소 8자리 이상 영문 대소문자, 숫자, 특수문자가 각각 1개 이상
+      const regExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+      if (!regExp.test(this.id)) {
+        this.alertPassword = '비밀번호는 8글자 이상의 영문 대소문자와 숫자, 특수문자의 조합이어야 합니다.';
+      } else {
+        this.alertPassword = '';
+      }
+    },
+    signUp() {
 
+    },
+  },
 }
 </script>
 
