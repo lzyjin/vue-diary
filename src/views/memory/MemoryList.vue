eb712676efc3baf14e57aa2c6eafed4e7b9846f7
lzyjin <1247y.j@gmail.com>
2023-02-27 22:46:59 +0900

    추억 리스트 페이지 마크업

--- src/views/memory/MemoryList.vue
+++ src/views/memory/MemoryList.vue
@@ -1,6 +1,101 @@
 <template>
   <div>
-    추억 페이지
+    <div class="memory-wrap">
+      <div class="top">
+        <div class="wrap">
+          <input type="text" name="" id="" placeholder="검색어를 입력해주세요">
+          <div class="btn-filter">
+            <span>필터</span>
+          </div>
+        </div>
+        <div class="wrap">
+          <p class="total">전체 n개 중 k개</p>
+          <div class="btn-add">등록</div>
+        </div>
+      </div>
+      <div class="list">
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">성수에서 재밌게 보낸날 - 자전거도 타고 맛있는 것도 먹고 한강도 갔다</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="img">
+            <img src="https://lipsum.app/random/200x200" alt="">
+          </div>
+          <div class="text">
+            <div class="edit"><i class="xi-ellipsis-h"></i></div>
+            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur deleniti dignissimos dolorum modi nobis quod soluta temporibus vero voluptatem. Aliquid cum distinctio dolores enim eum molestiae porro, sunt voluptatem!</p>
+            <p class="date">2023.02.27</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
