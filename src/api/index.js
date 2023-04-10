9c9664434af30a679161e46b565a5dfb760c6081
lzyjin <1247y.j@gmail.com>
2023-04-10 22:46:09 +0900

    api 호출 axios.request로 통일

--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,55 @@
 import axios from 'axios';
+import he from 'vue2-datepicker/locale/es/he';
 
 const prefix = 'http://121.161.237.50:50005/api/';
 
 const userApi = {
     up: `${prefix}user/up`,
-    in: `${prefix}user/in`
+    in: `${prefix}user/in`,
 };
 const calendarApi = {
     list: `${prefix}diary/list`,
     save: `${prefix}diary/save`,
-    delete: (diaryNo) => `${prefix}diary/remove/${diaryNo}`
+    delete: (diaryNo) => `${prefix}diary/remove/${diaryNo}`,
 };
 const memoryApi = {
     save: `${prefix}memory/save`,
     list: `${prefix}memory/`,
     detail: (memoryNo) => `${prefix}memory/${memoryNo}`,
-    delete: (memoryNo) => `${prefix}memory/${memoryNo}`
+    delete: (memoryNo) => `${prefix}memory/${memoryNo}`,
 };
 
-const post = (url, parameter) => {
-    return axios.post(url, parameter);
+const post = (url, parameter, headers) => {
+    return axios.request({
+        url: url,
+        method: 'post',
+        params: parameter,
+        headers: headers,
+    });
+};
+const patch = (url, parameter) => {
+    return axios.request({
+        url: url,
+        method: 'patch',
+        params: parameter,
+    });
 };
 const get = (url, parameter) => {
-    return axios.get(url, parameter);
+    return axios.request({
+        url: url,
+        method: 'get',
+        params: parameter,
+    });
 };
 const remove = (url, parameter) => {
-    return axios.delete(url, parameter);
+    return axios.request({
+        url: url,
+        method: 'delete',
+        params: parameter,
+    });
 };
+
+// TODO: axios 요청은 axios.request로 통일하기! 파라미터 보내는 형식 헷갈리지 않게!
 
 // 회원 - 회원가입
 export async function signUp(parameter = { userId: '', password: '' }) {
@@ -39,71 +62,64 @@ export async function signUp(parameter = { userId: '', password: '' }) {
 
 // 회원 - 로그인
 export async function signIn(userId, password) {
-    // console.log('api: ', userId, password);
     return get(userApi.in, { userId, password });
 }
 
 // 캘린더 - 목록
 export async function listCalendar(userNo, year, month) {
-    return await axios.get(calendarApi.list, {
-        params: {
-            userNo,
-            year,
-            month
-        }
+    return get(calendarApi.list, {
+        userNo,
+        year,
+        month,
     });
 }
 
 // 캘린더 - 등록
 // parameter: userNo, content, regDate
 export async function saveCalendar(payload) {
-    return await axios.post(calendarApi.save, {
+    return post(calendarApi.save, {
         userNo: payload.userNo,
         contents: payload.contents,
         regDate: payload.regDate,
-        diaryNo: payload.diaryNo
+        diaryNo: payload.diaryNo,
     });
 }
 
 // 캘린더 - 삭제
 // parameter:
 export async function removeCalendar(diaryNo) {
-    return await axios.delete(calendarApi.delete(diaryNo));
+    return delete calendarApi.delete(diaryNo);
 }
 
 // 추억 - 등록
 export async function saveMemory(payload) {
-    return await axios.post(memoryApi.save, payload, {
-        headers: {
-            'Content-Type': 'multipart/formed-data'
-        }
+    return post(memoryApi.save, payload, {
+        'Content-Type': 'multipart/formed-data',
     });
 }
 
 // 추억 - 목록
 export async function listMemory({ userNo, page, limit, address, category, startDate, endDate, offset, searchText }) {
-    return await axios.get(memoryApi.list, {
-        params: {
-            userNo,
-            page,
-            limit,
+    return get(memoryApi.list, {
+        userNo,
+        page,
+        limit,
 
-            address,
-            category,
-            startDate,
-            endDate,
-            offset,
-            searchText
-        }
+        address,
+        category,
+        startDate,
+        endDate,
+        offset,
+        searchText,
     });
 }
 
 // 추억 - 상세
 export async function getMemory(memoryNo) {
-    return await axios.get(memoryApi.detail(memoryNo));
+    return get(memoryApi.detail(memoryNo));
 }
 
 // 추억 - 삭제
 export async function deleteMemory(memoryNo) {
-    return await axios.delete(memoryApi.delete(memoryNo));
+    return delete memoryApi.delete(memoryNo);
 }
